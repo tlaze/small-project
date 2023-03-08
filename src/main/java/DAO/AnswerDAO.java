cb8de924c0c276c2c357b261211d49c5c1870991
tlaze <tomlazore@gmail.com>
2023-03-08 18:06:45 -0500

    added getAnswer_id method to AnswerDAO

--- src/main/java/DAO/AnswerDAO.java
+++ src/main/java/DAO/AnswerDAO.java
@@ -5,6 +5,29 @@ import java.sql.*;
 
 public class AnswerDAO {
 
-    public Answer get
+    public Answer getAnswer_id(int answer_id){
+        Connection con = ConnectionUtil.getConnection();
+
+        try{
+            String sql = "";
+
+            PreparedStatement ps = connection.prepareStatement(sql);
+
+            ps.setInt(1, answer_id);
+
+            ResultSet rs = ps.executeQuery();
+
+            while(rs.next()){
+                Answer getAnswer_id = new Answer(
+                        rs.getInt("answer_id")
+                );
+                return getAnswer_id;
+            }
+        }
+        catch(SQLException e){
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
 
 }
