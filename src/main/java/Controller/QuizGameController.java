f41fbb0ccfcd7c02d9d22f610003a25c0745ed35
tlaze <tomlazore@gmail.com>
2023-03-08 16:45:25 -0500

    added dao and controller files

--- src/main/java/Controller/QuizGameController.java
+++ src/main/java/Controller/QuizGameController.java
@@ -1,4 +1,19 @@
 package Controller;
+import io.javalin.Javalin;
+import io.javalin.http.Context;
 
 public class QuizGameController {
+
+    AnswerService answerService;
+    QuestionService questionService;
+
+    public QuizGameController(){
+        this.answerService = new AnswerService();
+        this.questionService = new QuestionService();
+    }
+
+    public Javalin startAPI(){
+        Javalin app = Javalin.create();
+        app.get("/question", this::getQuestionByIDHandler);
+    }
 }
