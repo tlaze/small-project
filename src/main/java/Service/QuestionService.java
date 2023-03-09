838e902201efb9c83d7ac437c9ad8f580c61deb6
Shkoh Hamasoor <shkoh.hamasoor@gmail.com>
2023-03-09 16:39:21 -0600

    updated the QuestionService

--- src/main/java/Service/QuestionService.java
+++ src/main/java/Service/QuestionService.java
@@ -1,5 +1,6 @@
 package Service;
 import java.util.List;
+import java.util.Scanner;
 
 import DAO.QuestionDAO;
 import Model.Question;
@@ -16,5 +17,49 @@ public class QuestionService {
     }
     public List<Question> getAllQuestions(){
         return questionDAO.getAllQuestions();
+    }
+    public Question getQuestionByID (int question_id){
+        return questionDAO.getQuestionByID(question_id);
+    }
+
+    public Question deleteQuestionID(int question_id) {
+        Question deleteQuestion_ID = questionDAO.getQuestionByID(question_id);
+        questionDAO.deleteQuestionByID(question_id);
+
+        if (deleteQuestion_ID != null) {
+            return deleteQuestion_ID;
+        } else {
+            return null;
+        }
+    }
+
+    public Question updateQuestionID(int question_id, Question question){
+        if(question.question_text!="" && question.question_text.length()<=255){
+
+            return questionDAO.updateQuestion(question_id, question);
+        }
+        else{
+            return null;
+        }
+    }
+
+    //if we can get it to work.
+    public Question addQuestion(Question question){
+
+            if (question.question_text.isBlank()) {
+                System.out.println("The Question cannot be blank");
+                return null;
+
+            } else if (question.question_text.length() >= 255) {
+                System.out.println("question cannot be over 255 characters");
+                return null;
+
+            } else if (question.answer_id <= 1) {
+                System.out.println("must assign it to an answer ID");
+            }
+
+        return questionDAO.createQuestion(question);
+
+
     }
 }
