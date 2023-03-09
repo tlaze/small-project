620936817aa3d1cd78bd8355a70d7d491c43335c
Shkoh Hamasoor <shkoh.hamasoor@gmail.com>
2023-03-09 13:52:30 -0600

    Created Service Class

--- src/main/java/Controller/QuizGameController.java
+++ src/main/java/Controller/QuizGameController.java
@@ -1,4 +1,1 @@
-package Controller;
 
-public class QuizGameController {
-}
--- src/main/java/Service/QuestionService.java
+++ src/main/java/Service/QuestionService.java
@@ -1,2 +1,20 @@
-package Service;public class QuestionService {
+package Service;
+import java.util.List;
+
+import DAO.QuestionDAO;
+import Model.Question;
+
+public class QuestionService {
+
+    public QuestionDAO questionDAO;
+
+    public QuestionService(){
+        questionDAO = new QuestionDAO();
+    }
+    public QuestionService(QuestionDAO questionDAO){
+        this.questionDAO = questionDAO;
+    }
+    public List<Question> getAllQuestions(){
+        return questionDAO.getAllQuestions();
+    }
 }
