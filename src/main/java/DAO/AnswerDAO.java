3cdb2682bcd2a3455809e4f0c470c55b20818247
tlaze <tomlazore@gmail.com>
2023-03-09 14:44:58 -0500

    created API running on localhost

--- src/main/java/DAO/AnswerDAO.java
+++ src/main/java/DAO/AnswerDAO.java
@@ -1,33 +1,32 @@
 package DAO;
 import Model.Answer;
-import Util.ConnectionUtil;
 import java.sql.*;
 
 public class AnswerDAO {
 
-    public Answer getAnswer_id(int answer_id){
-        Connection con = ConnectionUtil.getConnection();
-
-        try{
-            String sql = "";
-
-            PreparedStatement ps = connection.prepareStatement(sql);
-
-            ps.setInt(1, answer_id);
-
-            ResultSet rs = ps.executeQuery();
-
-            while(rs.next()){
-                Answer getAnswer_id = new Answer(
-                        rs.getInt("answer_id")
-                );
-                return getAnswer_id;
-            }
-        }
-        catch(SQLException e){
-            System.out.println(e.getMessage());
-        }
-        return null;
-    }
+//    public Answer getAnswer_id(int answer_id){
+//        Connection con = ConnectionUtil.getConnection();
+//
+//        try{
+//            String sql = "";
+//
+//            PreparedStatement ps = connection.prepareStatement(sql);
+//
+//            ps.setInt(1, answer_id);
+//
+//            ResultSet rs = ps.executeQuery();
+//
+//            while(rs.next()){
+//                Answer getAnswer_id = new Answer(
+//                        rs.getInt("answer_id")
+//                );
+//                return getAnswer_id;
+//            }
+//        }
+//        catch(SQLException e){
+//            System.out.println(e.getMessage());
+//        }
+//        return null;
+//    }
 
 }
