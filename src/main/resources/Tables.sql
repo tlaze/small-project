04e83cfe1b5381614fcdd9007caa6a19a3263c38
tlaze <tomlazore@gmail.com>
2023-03-09 20:16:34 -0500

    Questions and Answers show up on postman get request

--- src/main/resources/Tables.sql
+++ src/main/resources/Tables.sql
@@ -1,7 +1,7 @@
 --h2 is typically used to setup a test database, not a prod database.
 --first, drop your tables (to reset your database for testing)
 --then create your tables
-DROP TABLE if EXISTS question;
+DROP TABLE if EXISTS question cascade;
 DROP TABLE if EXISTS answer;
 
 --table to store our questions
@@ -9,24 +9,28 @@ CREATE TABLE question (
     question_id int NOT NULL,
     question_text varchar(255) NOT NULL,
     answer_id int,
-    PRIMARY KEY (question_id),
-    FOREIGN KEY (question_id) REFERENCES answer(answer_id)
+    PRIMARY KEY (question_id)
     );
 
 CREATE TABLE answer (
    answer_id int NOT NULL,
    choice_list varchar(255) NOT NULL,
-   correct_answer int,
-   PRIMARY KEY (answer_id),
-   FOREIGN KEY (correct_answer) REFERENCES question(answer_id)
+   correct_answer int
     );
 
 
-INSERT INTO question(question_id, question_text, answer_id) VALUES (1, 'hello', 2);
-INSERT INTO answer(answer_id, choice_list, correct_answer) VALUES (1, 'sadfas', 0);
-INSERT INTO answer(answer_id, choice_list, correct_answer) VALUES (2, 'gdagd', 0);
-INSERT INTO answer(answer_id, choice_list, correct_answer) VALUES (3, 'svcvc', 0);
-INSERT INTO answer(answer_id, choice_list, correct_answer) VALUES (4, 'sa343', 1);
+INSERT INTO question(question_id, question_text, answer_id) VALUES (1, 'question1', 1);
+INSERT INTO question(question_id, question_text, answer_id) VALUES (2, 'question2', 1);
+
+INSERT INTO answer(answer_id, choice_list, correct_answer) VALUES (1, 'answer1', 0);
+INSERT INTO answer(answer_id, choice_list, correct_answer) VALUES (1, 'answer2', 0);
+INSERT INTO answer(answer_id, choice_list, correct_answer) VALUES (1, 'answer3', 0);
+INSERT INTO answer(answer_id, choice_list, correct_answer) VALUES (1, 'answer4', 1);
+
+INSERT INTO answer(answer_id, choice_list, correct_answer) VALUES (2, 'answer1', 0);
+INSERT INTO answer(answer_id, choice_list, correct_answer) VALUES (2, 'answer2', 1);
+INSERT INTO answer(answer_id, choice_list, correct_answer) VALUES (2, 'answer3', 0);
+INSERT INTO answer(answer_id, choice_list, correct_answer) VALUES (2, 'answer4', 0);
 
 -- get all questions
 --  delete all questions if  answered
