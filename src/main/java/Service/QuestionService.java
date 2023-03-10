09f43be853a3fadbfb7737fc0a5b93c760e02be6
tlaze <tomlazore@gmail.com>
2023-03-10 13:05:52 -0500

    fixed merge issues. updateid working again

--- src/main/java/Service/QuestionService.java
+++ src/main/java/Service/QuestionService.java
@@ -14,11 +14,50 @@ public class QuestionService {
     public List<Question> getAllQuestions(){
         return questionDAO.getAllQuestions();
     }
-    public Question getQuestionByID(int question){
-        return questionDAO.getQuestionByID(question);
+    public Question getQuestionByID (int question_id){
+        return questionDAO.getQuestionByID(question_id);
     }
-    public Question updateQuestionByID(int questionID, Question question){
-        return questionDAO.updateQuestionByID(questionID, question);
+    public Question deleteQuestionID(int question_id) {
+        Question deleteQuestion_ID = questionDAO.getQuestionByID(question_id);
+        questionDAO.deleteQuestionByID(question_id);
+
+        if (deleteQuestion_ID != null) {
+            return deleteQuestion_ID;
+        } else {
+            return null;
+        }
     }
+    public Question updateQuestionByID(int question_id, Question question){
+        if(question.question_text!="" && question.question_text.length()<=255){
+
+            return questionDAO.updateQuestionByID(question_id, question);
+        }
+        else{
+            return null;
+        }
+    }
+
+//    //if we can get it to work.
+//    public Question addQuestion(Question question){
+//
+//            if (question.question_text.isBlank()) {
+//                System.out.println("The Question cannot be blank");
+//                return null;
+//
+//            } else if (question.question_text.length() >= 255) {
+//                System.out.println("question cannot be over 255 characters");
+//                return null;
+//
+//            } else if (question.answer_id <= 1) {
+//                System.out.println("must assign it to an answer ID");
+//            }
+//
+//        return questionDAO.createQuestion(question);
+//
+//
+//    }
+//    public Question updateQuestionByID(int questionID, Question question){
+//        return questionDAO.updateQuestionByID(questionID, question);
+//    }
 
 }
